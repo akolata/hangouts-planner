0e1a5e44a2543d37a2b4892b684db38f2f87dc42
Aleksander Kolata <olek.tbg.konta@gmail.com>
2023-01-17 00:47:27 +0100

    service hangout-service added

---
+++ apps/backend/services/hangout-service/src/test/java/pl/hangoutsplanner/service/hangout/HangoutServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.hangoutsplanner.service.hangout;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HangoutServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
