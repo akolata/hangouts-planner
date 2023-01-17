56f9ddbe33c9bc0001483aa04f38dd3e43e59ad3
Aleksander Kolata <olek.tbg.konta@gmail.com>
2023-01-18 00:27:51 +0100

    [MP-002] added API Gateway configuration

--- apps/backend/services/hangout-service/src/main/java/com/hangoutsplanner/service/hangoutservice/HangoutServiceApplication.java
+++ apps/backend/services/hangout-service/src/main/java/com/hangoutsplanner/service/hangoutservice/HangoutServiceApplication.java
@@ -1,26 +1,13 @@
 package com.hangoutsplanner.service.hangoutservice;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.core.env.Environment;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
 
-@RestController
 @SpringBootApplication
 public class HangoutServiceApplication {
 
-    @Autowired
-    private Environment env;
-
     public static void main(String[] args) {
         SpringApplication.run(HangoutServiceApplication.class, args);
-    }
-
-    @GetMapping("/test")
-    public String test() {
-        return env.getProperty("key");
     }
 
 }
---
+++ apps/backend/services/hangout-service/src/main/java/com/hangoutsplanner/service/hangoutservice/api/v1/HangoutsController.java
@@ -0,0 +1,14 @@
+package com.hangoutsplanner.service.hangoutservice.api.v1;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+class HangoutsController {
+
+    @GetMapping("/api/v1/hangouts/test")
+    public ResponseEntity<String> test() {
+        return ResponseEntity.ok("test");
+    }
+}
