e11001d227f44c38c30d6f98262cfb55f83e9b2c
Aleksander Kolata <olek.tbg.konta@gmail.com>
2023-01-17 22:59:44 +0100

    other services added - working on basic microservices architecture

---
+++ apps/backend/services/api-gateway/src/test/java/com/hangoutsplanner/service/apigateway/ApiGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hangoutsplanner.service.apigateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiGatewayApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
---
+++ apps/backend/services/hangout-service/src/test/java/com/hangoutsplanner/service/hangoutservice/HangoutServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hangoutsplanner.service.hangoutservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HangoutServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
